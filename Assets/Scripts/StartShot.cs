be98b77e5ba968312ebafe887f86eeb29ea4c7d4
FumioNakajima <fumio.aaa@gmail.com>
2020-09-04 10:59:31 +0900

    実験position

--- Assets/Scripts/StartShot.cs
+++ Assets/Scripts/StartShot.cs
@@ -8,6 +8,7 @@ public class StartShot : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        transform.position = new Vector3(0, 0, 0);
         // transform.eulerAngles = new Vector3(0, Random.Range(30, 120), 0);
         transform.eulerAngles = new Vector3(0, 90, 0);
         gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 500);
