f3e5e3a47e52eee0099099e1dda23b59716bd74a
FumioNakajima <fumio.aaa@gmail.com>
2020-09-09 11:56:42 +0900

    scriptの変更

--- Assets/Scripts/StartShot.cs
+++ Assets/Scripts/StartShot.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class StartShot : MonoBehaviour
 {
+    Rigidbody rigid;
 
     // Use this for initialization
     void Start()
     {
-        transform.position = new Vector3(0, 0, 0);
-        // transform.eulerAngles = new Vector3(0, Random.Range(30, 120), 0);
-        transform.eulerAngles = new Vector3(0, 90, 0);
-        gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 500);
+        transform.position = new Vector3(0, 0.4f, 0);
+        // transform.eulerAngles = new Vector3(45, Random.Range(30, 120), 0);
+        transform.eulerAngles = new Vector3(45, 90, 0);
+        rigid = gameObject.GetComponent<Rigidbody>();
+        rigid.AddForce(transform.forward * 500);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (transform.position.x > 8 || transform.position.x < -8)
+        {
+            transform.position = new Vector3(0, 0.4f, 0);
+        }
+        if (rigid.velocity.magnitude < 10f)
+        {
+            rigid.AddForce(transform.forward * 500);
+        }
     }
 }
