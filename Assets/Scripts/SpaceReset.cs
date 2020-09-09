8cc5bc2277d89f593bfe31890358e3b64cae1673
FumioNakajima <fumio.aaa@gmail.com>
2020-09-09 12:38:02 +0900

    リセット画面のスペースキーを有効に戻した

--- Assets/Scripts/SpaceReset.cs
+++ Assets/Scripts/SpaceReset.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SpaceReset : MonoBehaviour {
+public class SpaceReset : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.touchCount > 0)
-		{
-			foreach(Touch t in Input.touches)
-			{
-				if (t.phase == TouchPhase.Began)
-				{
-            		SceneManager.LoadScene("Title");
-				}
-			}
-		}
-        // if (Input.GetKey(KeyCode.Space)) {
-        //     SceneManager.LoadScene("Title");
-        // }
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            foreach (Touch t in Input.touches)
+            {
+                if (t.phase == TouchPhase.Began)
+                {
+                    SceneManager.LoadScene("Title");
+                }
+            }
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Title");
+        }
     }
 }
